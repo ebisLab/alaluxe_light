654de3bdc920b6a833719fd40d9bf2e553cf41b6
ebisLab <ebaiden@techie.com>
2021-04-01 22:20:29 -0400

    section 2 set up

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import {img} from './images'
 
 export default function Steps() {
     const [data, setData]=useState()
@@ -43,13 +44,95 @@ const filter_through=()=>{
         <>
         <section data-testid="steps-1" className="static section2">
         <div className="section2_centered">
-<h2 className="how_it_works">How It works</h2>
+<h2 className="how_it_works">New Collection</h2>
 <div className="filter_container">
-    {data && filter_through()}
+    {img.map(i=> <img src={i} style={{width:"200px", height:"280px" }}/>)}
 </div>
         
         </div> 
       </section>
+
+      <section data-testid="steps-1" className="static" 
+      style={{
+        //   top: "-539px", 
+      background:"#10060b"
+    }}
+      >
+        <div className="section2_centered" style={{color: "bisque"}}>
+<h2 className="how_it_works">New Collection</h2>
+<div className="filter_container sect3" style={{ gridTemplateColumns: "unset", width:"700px"}}>
+    <div style={{display:"inline-flex", height:"150px"}}>
+    <img src="./images/img_lamp2.jpg" 
+    style={{width: "250px",height: "330px",margin:"20px",position: "relative",
+    left: "-131px",top: "-140px",  boxShadow: "0px 0px 23px -1px black"}}/>
+
+        <div style={{width:"500px", margin:"20px", textAlign:"left",
+            position: "relative",
+            left:" -143px",
+            top:" -4px",
+            height: "fit-content",
+    }}>
+            <h2>Fixtures</h2>
+            <p>No matter what your personal style, Alaluxe has just the right light fixture for your living environment.
+            Element brings extrodianry, distinctive and unique light fixtures. A collection that blends
+            classic style with luxury quality.</p>
+            <span>Shop Online</span>
+            <hr/>
+            </div>
+    </div>
+
+    <div style={{display:"inline-flex", height:"300px"}}>
+        <div style={{width:"500px", margin:"20px", textAlign:"right"}}>
+            <h2>Features</h2>
+            <p>Our lights can add spiritual simplicity to your space, warm your living room or seperate two different areas in your 
+                dining room. 
+                </p>
+                <span>Shop Online</span>
+                <hr/>
+                </div>
+        <img src="./images/img_lamp3.jpg" style={{width: "250px",height: "330px",margin:"20px",  boxShadow: "0px 0px 23px -1px black"}}/>
+    </div>
+
+    <div style={{display:"inline-flex", height:"150px"}}>
+    <img src="./images/img_lamp4.jpg" style={{width: "250px",height: "330px",margin:"20px",position: "relative",
+    left: "-131px",top: "-140px",     boxShadow: "0px 0px 23px -1px black"}}/>
+        <div style={{width:"500px", margin:"20px", textAlign:"left",
+    position: "relative",
+    left:" -143px",
+    top:" -4px",
+    height: "fit-content"}}>
+            <h2>Lighting</h2>
+            <p>Our lights add character, warmth and value to any room n your house. We offer exceptional 
+                lights that will help you enhance the look of your home. You can get wood floors that look siny and new, or you can buy 
+                brand new light that looks like an antique.</p>
+                <span>Shop Online</span>
+                <hr/>
+            
+            </div>
+    </div>
+
+</div>
+        
+        </div> 
+      </section>
+
+
+      <section data-testid="steps-1" className="static" 
+      style={{
+          marginTop: "5%", 
+      background:"#10060b"
+    }}
+      >
+        <div className="section2_centered" style={{color: "bisque"}}>
+<h2 className="how_it_works">Contact us</h2>
+<form style={{display:"block"}}>
+    <input/>
+    <input />
+    <button>Submit</button>
+</form>
+</div>
+      </section>
+
         </>
     )
 }
---
+++ src/components/images.js
@@ -0,0 +1,2 @@
+export const img = ["./images/img_lamp3.jpg"
+, "./images/img_lamp4.jpg", "./images/img_lamp5.jpg", "./images/img_lamp2.jpg"]
